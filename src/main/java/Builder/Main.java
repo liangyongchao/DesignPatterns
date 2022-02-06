c1173b107bea9d8b0dc7fd085182273c5e7beec3
Charles Liang <liangyongchao@163.com>
2022-02-06 16:37:57 +0800

    ++ Builder pattern - add try/catch

--- src/main/java/Builder/Main.java
+++ src/main/java/Builder/Main.java
@@ -7,6 +7,18 @@ import java.util.Scanner;
 
 public class Main {
 
+    /**
+     * 定义：
+     *
+     *         将一个复杂对象的构建与它的表示分离，使得同样的构建过程可以创建不同的表示。生成器模式利用一个导演者对象和具体建造者对象一个一个地建造出所有的零件，从而建造出完整的对象。
+     *
+     * 四个要素：
+     *
+     *         Builder：生成器接口，定义创建一个Product对象所需要的各个部件的操作。
+     *         ConcreteBuilder：具体的生成器实现，实现各个部件的创建，并负责组装Product对象的各个部件，同时还提供一个让用户获取组装完成后的产品对象的方法。
+     *         Director：指导者，也被称导向者，主要用来使用Builder接口，以一个统一的过程来构建所需要的Product对象。
+     *         Product：产品，表示被生成器构建的复杂对象，包含多个部件。
+     */
     private static List<Integer> orderList = new ArrayList<Integer>();
 
     public static void main(String[]args){
@@ -53,24 +65,32 @@ public class Main {
             String id = scanner.next();
             ComboOrder comboOrder = build.composite(build.genPackage(id));
         }
+        scanner.close();
     }
 
     private static void nonComboOrder(Build build){
 
         Scanner scanner=new Scanner(System.in);
-
-        if (scanner.hasNext()) {
-            String prodId = scanner.next();
-            if(prodId.equalsIgnoreCase("#")){
-                NonComboOrder myOrder = new NonComboOrder();
-                Map list = build.genNonComboOrder(orderList);
-                myOrder.setOrder(list);
-                build.printNonComboOrder(myOrder);
-            }else{
-                orderList.add(Integer.valueOf(prodId));
-                nonComboOrder(build);
+        String prodId = "";
+        try{
+            if (scanner.hasNext()) {
+                prodId = scanner.next();
+                if(prodId.equalsIgnoreCase("#")){
+                    NonComboOrder myOrder = new NonComboOrder();
+                    Map list = build.genNonComboOrder(orderList);
+                    myOrder.setOrder(list);
+                    build.printNonComboOrder(myOrder);
+                }else{
+                    orderList.add(Integer.valueOf(prodId));
+                    nonComboOrder(build);
+                }
             }
+        }catch (Exception e){
+            String warning = String.format("Input ( %s ) is invalid.", prodId);
+            System.out.println(warning);
         }
+
+        scanner.close();
     }
 }
 
